51e58c32e0aa47aa9e107204e18275b6f26129af
C.C. <45786458+MajoSissi@users.noreply.github.com>
2026-06-27 21:56:11 +0800

    Update light-novel.sh

--- light-novel.sh
+++ light-novel.sh
@@ -3,7 +3,7 @@
 books=books
 # 单文件书源
 wget -O $books/Japan.json https://github.com/ZWolken/Light-Novel-Yuedu-Source/releases/latest/download/Japan_based_bookSource.json
-wget -O $books/Bot.json https://static.kksk.io/uuyr/b15e68c9cbdbad2000d52b7bc9137e7f.json
+wget -O $books/Bot.json https://static.kksk.io/uuyr/5d385f587070e0683afab2b17e939056.json
 # 仓库书源
 git clone https://github.com/jiwangyihao/source-j-legado source-j-legado
 cp source-j-legado/*.json $books
